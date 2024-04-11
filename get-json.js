7db95bb759b4d3054a2c0e5790b1be83d4b2c8d6
sarahcantrill <25324420@edgehill.ac.uk>
2024-04-11 09:35:59 +0000

    adding previous changes

---
+++ get-json.js
@@ -0,0 +1,6 @@
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+    OurRequest.open('module-1.json'+ 'module-2.json' + 'module-3.json' + pageCounter +'.json');
+    ourRequest.onload = function()
+});
