72ce75c7248e2ee17cd6c954caaef033d3721c9a
sarahcantrill <25324420@edgehill.ac.uk>
2024-04-24 13:41:33 +0000

    adding submit and reset button, creating form field for create assessment

--- get-json.js
+++ get-json.js
@@ -4,4 +4,4 @@ btn.addEventListener("click", function(){
     OurRequest.open('module-1.json'+ 'module-2.json' + 'module-3.json' + pageCounter +'.json');
     ourRequest.onload = function()
     
-    )};
+   )}; 
