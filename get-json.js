a7cb990e5d087283dbe012118e4fac99a70ab6da
sarahcantrill <25324420@edgehill.ac.uk>
2024-04-24 12:30:34 +0000

    adding buttons from use case diagram

--- get-json.js
+++ get-json.js
@@ -3,4 +3,5 @@ var btn = document.getElementById("btn");
 btn.addEventListener("click", function(){
     OurRequest.open('module-1.json'+ 'module-2.json' + 'module-3.json' + pageCounter +'.json');
     ourRequest.onload = function()
-});
+    
+)};
